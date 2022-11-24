95a94191a5c294cac91b607e2a6c27cd330c10ca
Olubusayo Amowe <amoweolubusayo@gmail.com>
2022-11-24 00:01:20 +0000

    initial commit

--- src/nftx-vault.ts
+++ src/nftx-vault.ts
@@ -1,52 +1,22 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  NFTXVault,
-  AdminChanged,
-  Upgraded
-} from "../generated/NFTXVault/NFTXVault"
-import { ExampleEntity } from "../generated/schema"
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { NewVault, NFTXVault } from "../generated/NFTXVault/NFTXVault";
+import { Upgraded } from "../generated/NFTXVaultFactory/NFTXVaultFactory";
+import { NFTXVaultTemplate } from "../generated/NFTXVaultTemplate/NFTXVaultTemplate";
+import { Vault } from "../generated/schema";
 
-export function handleAdminChanged(event: AdminChanged): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleNewVault(vault: NewVault): void {
+  let newVault = Vault.load(vault.address.toHexString());
+  let vaultTemplate = NFTXVaultTemplate.bind(vault.address);
+  let assetAddress = vaultTemplate.try_assetAddress();
+  let is1155 = vaultTemplate.try_is1155();
+  let vaultBalance = vaultTemplate.try_balanceOf(vault.address);
+  if (newVault == null) {
+    newVault = new Vault(vault.address.toHexString());
+    newVault.vaultId = vault.params.vaultId;
+    newVault.assetAddress = assetAddress.value;
+    newVault.vaultAddress = vault.address;
+    newVault.is1155 = is1155.value;
+    newVault.vaultBalance = vaultBalance.value;
+    newVault.save();
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.admin(...)
-  // - contract.implementation(...)
 }
-
-export function handleUpgraded(event: Upgraded): void {}
